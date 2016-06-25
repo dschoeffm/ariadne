69da2023559a0c307d14c65125180645fdb1e938
Dominik Schoeffmann <schoeffm@in.tum.de>
2016-06-25 08:53:13 +0200

    more complete DXR projection

--- src/dxr.cpp
+++ src/dxr.cpp
@@ -23,52 +23,57 @@ DXR::DXR(Table& table) : entries(table.get_sorted_entries()) {
 				continue;
 			}
 
-			for(; next(it) != expansion.end(); it++) {
+			bool mod_in_for = false;
+			for(; it != expansion.end(); it++) {
 				// Check if the prefix is between this range and the next
-				if(it->end < p_start && next(it)->start > p_end){
+				if(next(it) != expansion.end() && it->end < p_start && next(it)->start > p_end){
 					expansion.insert(next(it), p_entry);
+					mod_in_for = true;
 					break;
 				}
 				// Check if the prefix is at the beginning of the range
 				else if(p_start == it->start && p_end < it->end){
 					it->start = p_end +1;
 					expansion.insert(it, p_entry);
+					mod_in_for = true;
 					break;
 				}
 				// Check if the prefix is at the end of the range
 				else if(p_start > it->start && p_end == it->end){
 					it->end = p_start -1;
 					expansion.insert(next(it), p_entry);
+					mod_in_for = true;
 					break;
 				}
 				// Check if the prefix is enclosed inside
 				// a range in the list
 				else if((p_start >= it->start) && (p_end <= it->end)){
-
-					// TODO rewrite this section
-
 					// Set old entry to end before the new entry
-					if(it->start < p_start){
-						it->end = p.first -1;
-					}
+					it->end = p.first -1;
 
-					if(it->start < p_start){
-						it++;
-					}
-					expansion.insert(it, p_entry);
+					// Insert the new prefix
+					expansion.insert(next(it), p_entry);
 
 					// Create the entry to continue the old one
-					if(it->end > p_start){
-						p_entry.start = p_entry.end +1;
-						p_entry.end = it->end;
-						p_entry.next_hop = it->next_hop;
-						it++;
-						expansion.insert(it, p_entry);
-					}
+					p_entry.start = p_entry.end +1;
+					p_entry.end = it->end;
+					p_entry.next_hop = it->next_hop;
+					it++; // now points to the new prefix
+					expansion.insert(next(it), p_entry);
+					mod_in_for = true;
+					break;
 				}
 			}
 
-			// TODO end corner cases
+			// Only continue this iteration if there was no change yet
+			if(mod_in_for){
+				continue;
+			}
+
+			// Check if there is a hole at the end
+			if(prev(it)->end < p_start){
+				expansion.insert(it, p_entry);
+			}
 		}
 	}
 
