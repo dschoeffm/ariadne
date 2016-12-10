dd0fa8a5d2b7b49b351dc45d83776759440cb087
Dominik Schoeffmann <schoeffm@in.tum.de>
2016-12-10 20:24:34 +0100

    add Manager::startWorkerThreads()

--- src/manager.cpp
+++ src/manager.cpp
@@ -160,5 +160,10 @@ void Manager::fillNetLink() {
 	mnl_socket_close(nl);
 }
 
-
+void Manager::startWorkerThreads(){
+	for(unsigned i=0; thread::hardware_concurrency()-1; i++){
+		worker.push_back(new Worker(cur_lpm, arpTable.getCurrentTable(),
+			in_rings[i], out_rings[i], interface_macs, own_IPs));
+	}
+};
 
