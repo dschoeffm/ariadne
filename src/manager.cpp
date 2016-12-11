36efc3b17d5c5d8871b0361c92fe5ccf694c9193
Dominik Schoeffmann <schoeffm@in.tum.de>
2016-12-11 10:45:15 +0100

    add Manager::fillNetLink()

--- src/manager.cpp
+++ src/manager.cpp
@@ -1,5 +1,26 @@
 #include "manager.hpp"
 
+using namespace std;
+
+std::shared_ptr<std::vector<interface>> fillNetLink(){
+	shared_ptr<vector<interface>> interfaces = Netlink::getAllInterfaces();
+	sort(interfaces->begin(), interfaces->end());
+	uint32_t max_index = interfaces->back().index;
+	if(max_index != interfaces->size()){
+		for(uint32_t index=0; index<max_index; index++){
+			auto it = find_if(interfaces->begin(), interfaces->end(),
+					[index](interface& i){
+						return i == index;
+					});
+
+			if (it == interfaces->end()){
+				interfaces->emplace(interfaces->end(), index);
+			}
+		}
+	}
+	sort(interfaces->begin(), interfaces->end());
+	return interfaces;
+};
 
 void Manager::startWorkerThreads(){
 	for(unsigned i=0; thread::hardware_concurrency()-1; i++){
