63c084f1f33f824f96a6238f6fba55b735fa4386
Dominik Schoeffmann <schoeffm@in.tum.de>
2017-03-15 09:52:57 +0100

    switch to BasicTrie

--- include/config.hpp
+++ include/config.hpp
@@ -5,7 +5,7 @@
 #include "naive.hpp"
 
 // Which LPM implmentation to use
-using LPM=Naive;
+using LPM=BasicTrie;
 
 // Fixed ring size
 constexpr auto RING_SIZE = 1024;
@@ -16,4 +16,4 @@ constexpr auto ARP_WAIT_MILLIS = 1000;
 // Maximum backlog in a worker
 constexpr auto WORKER_MAX_BACKLOG = 256;
 
-#endif /* FRAME_HPP */
+#endif /* CONFIG_HPP */
